71b3016b5189eca0af7131e74004807fb448a5f4
JamelSalem <salemjamalfsg.com>
2021-02-17 16:35:18 +0100

    test front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'dashboard', component:HeaderComponent,children:[
+{path:'home',component:HomeComponent},
+{path:'about',component:AboutComponent}]},
+{path:'login',component:LoginComponent},
+{path:'**',redirectTo:'/dashboard/home',pathMatch:'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  
+  imports: [
+    BrowserModule,
+
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
